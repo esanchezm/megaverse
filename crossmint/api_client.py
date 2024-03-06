a2524549c5a60e4a9b86d3ac33db3ae3c4c8cdc9
Esteban Sánchez <esteban.sanchez@gmail.com>
2024-03-06 11:54:59 +0100

    Phase 2

--- crossmint/api_client.py
+++ crossmint/api_client.py
@@ -1,35 +1,58 @@
-from apiclient import APIClient, QueryParameterAuthentication, NoAuthentication, JsonResponseHandler, JsonRequestFormatter
+from enum import Enum
+
+import tenacity
+from apiclient import (
+    APIClient,
+    JsonRequestFormatter,
+    JsonResponseHandler,
+    NoAuthentication,
+)
 from apiclient.error_handlers import ErrorHandler
 from apiclient.request_strategies import RequestStrategy
+from apiclient.retrying import retry_if_api_request_error
 from apiclient.utils.typing import OptionalDict
+
+retry_request = tenacity.retry(
+    retry=retry_if_api_request_error(status_codes=[429, 500, 501, 503]),
+    wait=tenacity.wait_fixed(2),
+    stop=tenacity.stop_after_attempt(5),
+    reraise=True,
+)
 
 
 class CrossmintAuthenticationStrategy(RequestStrategy):
+    """
+    Authentication strategy for Crossmint API. It adds the candidateId to the request data for POST and DELETE.
+    """
 
     def __init__(self, candidate_id: str):
         self.candidate_id = candidate_id
         super().__init__()
 
+    @retry_request
     def post(self, endpoint: str, data: dict, params: OptionalDict = None, **kwargs):
         data.update({"candidateId": self.candidate_id})
         super().post(endpoint, data, params, **kwargs)
 
+    @retry_request
     def delete(self, endpoint: str, params: OptionalDict = None, **kwargs):
         kwargs["data"].update({"candidateId": self.candidate_id})
         super().delete(endpoint, params, **kwargs)
 
 
-
 class BaseCrossmintClient(APIClient):
+    """
+    Base client for Crossmint API. It sets the authentication strategy to CrossmintAuthenticationStrategy and the
+    response handler to JsonResponseHandler.
+    """
+
     DEFAULT_BASE_URL = "https://challenge.crossmint.io"
 
     def __init__(self, candidate_id: str, base_url=None):
         self.candidate_id = candidate_id
         self.base_url = base_url or self.DEFAULT_BASE_URL
         super(BaseCrossmintClient, self).__init__(
-            request_strategy=CrossmintAuthenticationStrategy(
-                self.candidate_id
-            ),
+            request_strategy=CrossmintAuthenticationStrategy(self.candidate_id),
             response_handler=JsonResponseHandler,
             request_formatter=JsonRequestFormatter,
             error_handler=ErrorHandler,
@@ -37,25 +60,38 @@ class BaseCrossmintClient(APIClient):
 
 
 class MapClient(BaseCrossmintClient):
+    """
+    Client for the map endpoint of the Crossmint API. It has no authentication
+    """
 
     def __init__(self, candidate_id: str, base_url=None):
         super().__init__(candidate_id, base_url)
         self.set_authentication_method(NoAuthentication())
         self.rest_url = f"{self.base_url}/api/map/{self.candidate_id}"
 
+    @retry_request
     def get_status(self):
         return self.get(self.rest_url)
 
+    @retry_request
     def get_goal(self):
         return self.get(f"{self.rest_url}/goal")
 
 
 class PolyanetClient(BaseCrossmintClient):
+    """
+    Client for the polyanet endpoint of the Crossmint API.
+
+    It has the following methods:
+    - set(row: int, col: int): Set a polyanet at the given row and column
+    - clean(row: int, col: int): Clean the polyanet at the given row and column
+    """
 
     def __init__(self, candidate_id: str, base_url=None):
         super().__init__(candidate_id, base_url)
         self.rest_url = f"{self.base_url}/api/polyanets"
 
+    @retry_request
     def set(self, row: int, col: int):
         return self.post(
             self.rest_url,
@@ -65,6 +101,113 @@ class PolyanetClient(BaseCrossmintClient):
             },
         )
 
+    @retry_request
+    def clean(self, row: int, col: int):
+        return self.delete(
+            self.rest_url,
+            data={
+                "row": row,
+                "column": col,
+            },
+        )
+
+
+class Color(Enum):
+    """
+    Enum for the color of the soloon.
+    """
+
+    RED = "red"
+    BLUE = "blue"
+    PURPLE = "purple"
+    WHITE = "white"
+
+    @classmethod
+    def parse(cls, value: str):
+        try:
+            return cls[value.upper()]
+        except KeyError as e:
+            raise ValueError(f"Unknown color: {value}", e)
+
+
+class SoloonClient(BaseCrossmintClient):
+    """
+    Client for the soloon endpoint of the Crossmint API.
+
+    It has the following methods:
+    - set(row: int, col: int, color: Color): Set a soloon at the given row and column with the given color
+    - clean(row: int, col: int): Clean the soloon at the given row and column
+    """
+
+    def __init__(self, candidate_id: str, base_url=None):
+        super().__init__(candidate_id, base_url)
+        self.rest_url = f"{self.base_url}/api/soloons"
+
+    @retry_request
+    def set(self, row: int, col: int, color: Color):
+        return self.post(
+            self.rest_url,
+            data={
+                "row": row,
+                "column": col,
+                "color": color.value,
+            },
+        )
+
+    @retry_request
+    def clean(self, row: int, col: int):
+        return self.delete(
+            self.rest_url,
+            data={
+                "row": row,
+                "column": col,
+            },
+        )
+
+
+class Direction(Enum):
+    """
+    Enum for the direction of the cometh.
+    """
+
+    UP = "up"
+    DOWN = "down"
+    LEFT = "left"
+    RIGHT = "right"
+
+    @classmethod
+    def parse(cls, value: str):
+        try:
+            return cls[value.upper()]
+        except KeyError as e:
+            raise ValueError(f"Unknown direction: {value}", e)
+
+
+class ComethClient(BaseCrossmintClient):
+    """
+    Client for the cometh endpoint of the Crossmint API.
+
+    It has the following methods:
+    - set(row: int, col: int, direction: Direction): Set a cometh at the given row and column with the given direction
+    - clean(row: int, col: int): Clean the cometh at the given row and column
+    """
+
+    def __init__(self, candidate_id: str, base_url=None):
+        super().__init__(candidate_id, base_url)
+        self.rest_url = f"{self.base_url}/api/comeths"
+
+    @retry_request
+    def set(self, row: int, col: int, direction: Direction):
+        return self.post(
+            self.rest_url,
+            data={
+                "row": row,
+                "column": col,
+                "direction": direction.value,
+            },
+        )
+
+    @retry_request
     def clean(self, row: int, col: int):
         return self.delete(
             self.rest_url,
--- crossmint/commands.py
+++ crossmint/commands.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
 import logging
+
 from cleo.commands.command import Command
 from cleo.helpers import argument, option
-from .api_client import MapClient, PolyanetClient
+
+from .api_client import (
+    Color,
+    ComethClient,
+    Direction,
+    MapClient,
+    PolyanetClient,
+    SoloonClient,
+)
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -26,13 +35,38 @@ class ReconcileCommand(Command):
         ),
     ]
 
+    def is_space(self, value: str):
+        """
+        Check if the value is a space.
+        """
+        return value == "SPACE"
+
+    def is_polyanet(self, value: str):
+        """
+        Check if the value is a polyanet.
+        """
+        return value == "POLYANET"
+
+    def is_soloon(self, value: str):
+        """
+        Check if the value is a soloon.
+        """
+        return "_SOLOON" in value
+
+    def is_cometh(self, value: str):
+        """
+        Check if the value is a cometh.
+        """
+        return "_COMETH" in value
+
     def handle(self):
         candidate_id = self.argument("candidate_id")
 
         map_client = MapClient(candidate_id, self.option("url"))
         polyanet_client = PolyanetClient(candidate_id, self.option("url"))
+        soloon_client = SoloonClient(candidate_id, self.option("url"))
+        cometh_client = ComethClient(candidate_id, self.option("url"))
 
-        map_client = MapClient(candidate_id, self.option("url"))
         current_map = map_client.get_status().get("map", {}).get("content", [])
         goal_map = map_client.get_goal().get("goal", [])
 
@@ -40,20 +74,35 @@ class ReconcileCommand(Command):
         logger.info(f"Reconciling map for candidate {candidate_id}")
 
         for row in range(len(goal_map)):
-            for col in range(len(goal_map[row])):
-                if current_map[row][col] is None:
-                    current_map[row][col] = "SPACE"
+            for column in range(len(goal_map[row])):
+                if current_map[row][column] is None:
+                    current_map[row][column] = "SPACE"
 
-                if current_map[row][col] == goal_map[row][col]:
-                    logger.info(f"Map already reconciled at {row}, {col}")
+                if current_map[row][column] == goal_map[row][column]:
+                    logger.info(f"Map already reconciled at {row}, {column}")
                     continue
 
-                if goal_map[row][col] == "SPACE":
-                    logger.info(f"Cleaning polyanet at {row}, {col}")
-                    polyanet_client.clean(row, col)
-                elif goal_map[row][col] == "POLYANET":
-                    logger.info(f"Setting polyanet at {row}, {col}")
-                    polyanet_client.set(row, col)
+                if self.is_space(goal_map[row][column]):
+                    logger.info(f"Cleaning {row}, {column}")
+                    polyanet_client.clean(row, column)
+                    soloon_client.clean(row, column)
+                    cometh_client.clean(row, column)
+                elif self.is_polyanet(goal_map[row][column]):
+                    logger.info(f"Setting polyanet at {row}, {column}")
+                    polyanet_client.set(row, column)
+                elif self.is_soloon(goal_map[row][column]):
+                    color = Color(goal_map[row][column].split("_")[0].lower())
+                    logger.info(
+                        f"Setting soloon at {row}, {column} with color {color.value}"
+                    )
+                    soloon_client.set(row, column, color)
+                elif self.is_cometh(goal_map[row][column]):
+                    direction = Direction(goal_map[row][column].split("_")[0].lower())
+                    logger.info(
+                        f"Setting cometh at {row}, {column} with direction {direction.value}"
+                    )
+                    cometh_client.set(row, column, direction)
                 else:
-                    raise RuntimeError(f"Unknown map value: {goal_map[row][col]}")
-
+                    logger.error(
+                        f"Unknown map value: {goal_map[row][column]}, skipping"
+                    )
